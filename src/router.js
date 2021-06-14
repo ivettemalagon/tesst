133b91b7ef74988d85ed13ffefa2a774e185e7c9
ivettemalagon <71296531+ivettemalagon@users.noreply.github.com>
2021-06-14 03:35:15 -0500

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import vueRouter from 'vue-router'
-import User from './components/User'
-import UserAuth from './components/UserAuth'
-import Inventory from './components/Inventory'
-import Search from './components/Search'
-import Modify from './components/Modify'
-import App from './App'
+import User from './components/User.vue'
+import UserAuth from './components/UserAuth.vue'
+import Inventory from './components/Inventory.vue'
+import Search from './components/Search.vue'
+import Modify from './components/Modify.vue'
+import App from './App.vue'
 
 const router = new vueRouter({
     mode: 'history',
